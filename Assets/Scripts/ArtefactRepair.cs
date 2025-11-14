a3cf4720449062e0362e752a8e6b7f9418366572
AbdullahiKJ <154251356+AbdullahiKJ@users.noreply.github.com>
2025-11-14 15:59:45 +0000

    feat: add mini game completion/error feedback

--- Assets/Scripts/ArtefactRepair.cs
+++ Assets/Scripts/ArtefactRepair.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
@@ -5,16 +6,18 @@ public class ArtefactRepair : MonoBehaviour
 {
     [SerializeField] GameObject fracturedPrefab;
     GameObject fracturedInstance;
-    Vector3 initialRotation;
     [SerializeField] GameObject completedPrefab;
     GameObject completedInstance;
+    [SerializeField] GameObject particleSystemPrefab;
     // Game object that holds the puzzle slots
     GameObject slotInstance;
     [SerializeField] Transform completedPosition;
     [SerializeField] Transform fracturedPosition;
     [SerializeField] float spacing;
     GameObject[] pieceList;
+    GameObject[] slotList;
     int pieceCount;
+    int correctPiecees = 0;
     [SerializeField] float socketRadius = 0.1f;
 
     void Start()
@@ -26,6 +29,7 @@ public class ArtefactRepair : MonoBehaviour
         // Get all the individual pieces
         pieceCount = fracturedInstance.transform.childCount;
         pieceList = GetAllChildren(fracturedInstance.transform);
+        slotList = new GameObject[pieceCount];
 
         // Create an empty game object where the pieces will go
         slotInstance = new GameObject("slots");
@@ -34,13 +38,20 @@ public class ArtefactRepair : MonoBehaviour
         // Create child game objects for each slot and make them children of the slot instance
         for (int i = 0; i < pieceCount; i++)
         {
-            GameObject slot = new GameObject();
+            GameObject slot = new GameObject("slot " + i);
             slot.transform.SetParent(slotInstance.transform);
             slot.transform.position = pieceList[i].transform.position;
             slot.transform.rotation = pieceList[i].transform.rotation;
 
+            // Add the slot to the slot list
+            slotList[i] = slot;
+
             // Add the XR socket interactor component
-            slot.AddComponent<XRSocketInteractor>();
+            XRSocketInteractor socket = slot.AddComponent<XRSocketInteractor>();
+
+            // Subscribe to the select entered and exited events
+            socket.selectEntered.AddListener(OnObjectSnap);
+            socket.selectExited.AddListener(OnObjectUnsnap);
 
             // Add the sphere collider
             SphereCollider sphere = slot.AddComponent<SphereCollider>();
@@ -48,6 +59,7 @@ public class ArtefactRepair : MonoBehaviour
             sphere.radius = socketRadius;
         }
 
+        // Reposition the fractured instance to the other side of the table
         fracturedInstance.transform.position = fracturedPosition.position;
 
         // Attach a mesh collider to each child and the xr grab interactable script
@@ -63,12 +75,6 @@ public class ArtefactRepair : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     // Get all children from the parent game object
     GameObject[] GetAllChildren(Transform root)
     {
@@ -79,5 +85,82 @@ public class ArtefactRepair : MonoBehaviour
             allChildren[i] = root.GetChild(i).gameObject;
         }
         return allChildren;
+    }
+
+    void CompletePuzzle()
+    {
+        // Instantiate the completed prefab as a child of the table at the completed position
+        completedInstance = Instantiate(completedPrefab, transform);
+        completedInstance.transform.position = completedPosition.position;
+
+        // todo: play completion sound or provide feedback for puzzle completion
+
+        // Instantiate and play particle effect
+        Instantiate(particleSystemPrefab, completedPosition.position, Quaternion.identity);
+
+        // Destroy the fractured instance and slot instance
+        Destroy(fracturedInstance);
+        Destroy(slotInstance);
+    }
+
+    bool CompareIndices(GameObject piece, GameObject slot)
+    {
+        // Check if the piece is in the correct slot
+        if (Array.IndexOf(pieceList, piece) == Array.IndexOf(slotList, slot))
+        {
+            // Increase the correct pieces count
+            correctPiecees++;
+
+            // Check if all pieces are in the correct slots
+            if (correctPiecees >= pieceCount)
+            {
+                CompletePuzzle();
+            }
+            return true;
+        }
+        return false;
+    }
+
+    void OnObjectSnap(SelectEnterEventArgs args)
+    {
+        // Get the xr grab interactable component from the snapped object
+        XRGrabInteractable grabbedObject = args.interactableObject as XRGrabInteractable;
+
+        bool correctPiece = CompareIndices(grabbedObject.gameObject, args.interactorObject.transform.gameObject);
+
+        // Disable the xr interactable/interactor components if the piece is correct to prevent further interaction
+        if (correctPiece)
+        {
+            // Place the piece exactly at the slot position and rotation
+            grabbedObject.transform.position = args.interactorObject.transform.position;
+            grabbedObject.transform.rotation = args.interactorObject.transform.rotation;
+
+            // Disable the socket interactor
+            XRSocketInteractor socket = args.interactorObject as XRSocketInteractor;
+            socket.enabled = false;
+
+            // Disable the grab interactableon the piece
+            grabbedObject.enabled = false;
+
+            // Disable the rigidbody physics
+            Rigidbody rb = grabbedObject.gameObject.GetComponent<Rigidbody>();
+            rb.isKinematic = false;
+            rb.useGravity = false;
+
+            // Freeze the piece rotation and transform
+            rb.constraints = RigidbodyConstraints.FreezeAll;
+
+            // todo: Play success sound or provide feedback for correct placement
+        }
+        else
+        {
+            // todo: Play error sound or provide feedback for incorrect placement
+        }
+    }
+
+    void OnObjectUnsnap(SelectExitEventArgs args)
+    {
+        // Get the xr grab interactable component from the snapped object
+        XRGrabInteractable grabbedObject = args.interactableObject as XRGrabInteractable;
     }
 }
