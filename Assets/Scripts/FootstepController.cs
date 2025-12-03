46cd470730f365cc41ffe513e76a5c44e0161953
Oluwaremi Oluwole <polymathwole@gmail.com>
2025-12-03 13:44:34 +0000

    Implement sound play on movement

---
+++ Assets/Scripts/FootstepController.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class FootstepController : MonoBehaviour
+{
+    private float minSpeed = 0.1f;
+
+    [SerializeField]
+    private AudioClip footstepClip;
+
+    [SerializeField]
+    private InputActionReference moveAction;
+
+    private float stepInterval = 0.5f; // time between steps
+    private float stepTimer = 0f;
+
+    void OnEnable()
+    {
+        moveAction.action.Enable();
+    }
+
+    void OnDisable()
+    {
+        moveAction.action.Disable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 input = moveAction.action.ReadValue<Vector2>();
+        bool isMoving = input.sqrMagnitude > minSpeed;
+
+        if (isMoving)
+        {
+            stepTimer += Time.deltaTime;
+
+            if (stepTimer >= stepInterval)
+            {
+                SoundEffectsManager.Instance.PlaySound(footstepClip, transform);
+                stepTimer = 0f;
+            }
+        }
+        else
+        {
+            stepTimer = stepInterval;
+        }
+    }
+}
+
