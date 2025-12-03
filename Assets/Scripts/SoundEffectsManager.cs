8a9533a390139ae46c02dcd6173901e07ddd9772
Oluwaremi Oluwole <polymathwole@gmail.com>
2025-12-03 16:46:45 +0000

    Implement music playing continuously on game start

--- Assets/Scripts/SoundEffectsManager.cs
+++ Assets/Scripts/SoundEffectsManager.cs
@@ -6,6 +6,8 @@ public class SoundEffectsManager : MonoBehaviour
 
     [SerializeField]
     private AudioSource soundEffectObject;
+    [SerializeField]
+    private AudioClip music;
 
     void Awake()
     {
@@ -13,24 +15,27 @@ public class SoundEffectsManager : MonoBehaviour
             Instance = this;
         else
             Destroy(gameObject);
+
+        PlaySound(music, .7f, true);
     }
 
-    public void PlaySound(AudioClip audioClip, float volume = 1f)
+    public void PlaySound(AudioClip audioClip, float volume = 1f, bool loop = false)
     {
         // instantiate sound FX prefab
         AudioSource audioSource = Instantiate(soundEffectObject, Camera.main.transform.position, Quaternion.identity);
-        audioSource.transform.parent = Camera.main.transform;
 
         // assign audio clip
         audioSource.clip = audioClip;
 
         // set volume
         audioSource.volume = volume;
+        audioSource.loop = loop;
 
         // play sound
         audioSource.Play();
 
         // destroy clip after it has played
-        Destroy(audioSource.gameObject, audioSource.clip.length);
+        if (!loop)
+            Destroy(audioSource.gameObject, audioSource.clip.length);
     }
 }
