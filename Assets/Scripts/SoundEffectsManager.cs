b80f00cdc3059d324a197fb050644e50b9d71efd
Oluwaremi Oluwole <polymathwole@gmail.com>
2025-12-03 09:21:17 +0000

    Implement sound effects manager logic

---
+++ Assets/Scripts/SoundEffectsManager.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class SoundEffectsManager : MonoBehaviour
+{
+    public static SoundEffectsManager Instance;
+
+    [SerializeField]
+    private AudioSource soundEffectObject;
+
+    void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        /* else
+            Destroy(gameObject); */
+    }
+
+    public void PlaySound(AudioClip audioClip, Transform transform, float volume = 1f)
+    {
+        // instantiate sound FX prefab
+        AudioSource audioSource = Instantiate(soundEffectObject, transform.position, Quaternion.identity);
+
+        // assign audio clip
+        audioSource.clip = audioClip;
+
+        // set volume
+        audioSource.volume = volume;
+
+        // play sound
+        audioSource.Play();
+
+        // destroy clip after it has played
+        Destroy(audioSource.gameObject, audioSource.clip.length);
+    }
+}
